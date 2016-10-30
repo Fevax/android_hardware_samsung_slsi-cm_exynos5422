40327573e5a8da2251216095b69ecaa2afb8bb21
somadsul <somnathadsul@gmail.com>
2016-10-30 11:50:45 +0530

    gralloc: ifdef GRALLOC_USAGE_GPU_BUFFER

--- gralloc/gralloc.cpp
+++ gralloc/gralloc.cpp
@@ -121,9 +121,11 @@ static unsigned int _select_heap(int usage)
     else
         heap_mask = ION_HEAP_SYSTEM_MASK;
 
+#ifdef GRALLOC_USAGE_GPU_BUFFER
     if (usage & GRALLOC_USAGE_GPU_BUFFER)
         heap_mask = ION_HEAP_EXYNOS_CONTIG_MASK;
-
+#endif
+	
     return heap_mask;
 }
 
@@ -206,6 +208,7 @@ static int gralloc_alloc_rgb(int ionfd, int w, int h, int format, int usage,
     err = ion_alloc_fd(ionfd, size, alignment, heap_mask, ion_flags,
                        &fd);
     if (err) {
+#ifdef GRALLOC_USAGE_GPU_BUFFER
         if (usage & GRALLOC_USAGE_GPU_BUFFER) {
             usage &= ~GRALLOC_USAGE_GPU_BUFFER;
             heap_mask = _select_heap(usage);
@@ -215,6 +218,7 @@ static int gralloc_alloc_rgb(int ionfd, int w, int h, int format, int usage,
                 return err;
         }
         else
+#endif
             return err;
     }
     *hnd = new private_handle_t(fd, size, usage, w, h, format, *stride,
@@ -377,9 +381,11 @@ static int gralloc_alloc(alloc_device_t* dev,
     gralloc_module_t* module = reinterpret_cast<gralloc_module_t*>
         (dev->common.module);
 
+#ifdef GRALLOC_USAGE_GPU_BUFFER
     if ((usage & GRALLOC_USAGE_GPU_BUFFER) && (w*h != (m->xres)*(m->yres)))
         usage &= ~GRALLOC_USAGE_GPU_BUFFER;
-
+#endif
+	
     err = gralloc_alloc_rgb(m->ionfd, w, h, format, usage, ion_flags, &hnd,
                             &stride);
     if (err)
