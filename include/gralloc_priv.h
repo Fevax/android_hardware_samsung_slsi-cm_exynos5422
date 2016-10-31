exynos5420/android_hardware_samsung_slsi-cm_exynos5420

gralloc: Define sNumInts on a per-device basis
* Each device has a different value, so let's
  have each device define the correct value

Change-Id: I67fdc2d3a94b66845c419a1502010fce94a4a1aa
Signed-off-by: Paul Keith <javelinanddart@gmail.com>

include/gralloc_priv.h
@@ -102,7 +102,7 @@ struct private_handle_t {
 
 #ifdef __cplusplus
     static const int sNumFds = 3;
-    static const int sNumInts = 34;
+    static const int sNumInts = BOARD_CAMERA_SNUMINTS;
     static const int sMagic = 0x3141592;
 
     private_handle_t(int fd, int size, int flags) :

Fevax/android_hardware_samsung_slsi-cm_exynos5422

include/gralloc_priv.h
@@ -102,7 +102,7 @@ struct private_handle_t {
 
 #ifdef __cplusplus
     static const int sNumFds = 3;
-    static const int sNumInts = 24;
+    static const int sNumInts = CAMERA_SNUMINTS;
     static const int sMagic = 0x3141592;
 
     private_handle_t(int fd, int size, int flags) :