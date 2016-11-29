d6766322acb70dfc1b6e1a61dfd19f191609565c
somadsul <somnathadsul@gmail.com>
2016-11-29 14:01:16 +0530

    gralloc: add ifdefs around GRALLOC_USAGE_GPU_BUFFER
    These were meant for bringup purposes and test cases, but every
    device should have this definition and a framebuffer device
    (S3C or DECON) which supports this.

--- gralloc/gralloc.cpp
+++ gralloc/gralloc.cpp
@@ -128,10 +128,12 @@ static unsigned int _select_heap(int usage)
         heap_mask = ION_HEAP_EXYNOS_CONTIG_MASK;
     else
         heap_mask = ION_HEAP_SYSTEM_MASK;
-
+	
+#ifdef GRALLOC_USAGE_GPU_BUFFER
     if (usage & GRALLOC_USAGE_GPU_BUFFER)
         heap_mask = ION_HEAP_EXYNOS_CONTIG_MASK;
-
+#endif
+	
     return heap_mask;
 }
 
@@ -216,6 +218,7 @@ static int gralloc_alloc_rgb(int ionfd, int w, int h, int format, int usage,
     err = ion_alloc_fd(ionfd, size, alignment, heap_mask, ion_flags,
                        &fd);
     if (err) {
+#ifdef GRALLOC_USAGE_GPU_BUFFER
         if (usage & GRALLOC_USAGE_GPU_BUFFER) {
             usage &= ~GRALLOC_USAGE_GPU_BUFFER;
             heap_mask = _select_heap(usage);
@@ -225,6 +228,7 @@ static int gralloc_alloc_rgb(int ionfd, int w, int h, int format, int usage,
                 return err;
         }
         else
+#endif
             return err;
     }
     *hnd = new private_handle_t(fd, size, usage, w, h, format, *stride,
@@ -387,9 +391,11 @@ static int gralloc_alloc(alloc_device_t* dev,
     gralloc_module_t* module = reinterpret_cast<gralloc_module_t*>
         (dev->common.module);
 
+#ifdef GRALLOC_USAGE_GPU_BUFFER
     if ((usage & GRALLOC_USAGE_GPU_BUFFER) && (w*h != (m->xres)*(m->yres)))
         usage &= ~GRALLOC_USAGE_GPU_BUFFER;
-
+#endif
+	
     err = gralloc_alloc_rgb(m->ionfd, w, h, format, usage, ion_flags, &hnd,
                             &stride);
     if (err)
